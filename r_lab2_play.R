67bc139063abc27758f0d2cb504963f7e860cfc1
Richard Barad <richard.barad@gmail.com>
2023-09-11 23:37:34 -0400

    Create Repository
    Add Michael's R Markdown and HTML files from Philadelphia and add my file where I was playing around with things.

---
+++ r_lab2_play.R
@@ -0,0 +1,53 @@
+library(tidycensus)
+library(tidyverse)
+library(kableExtra)
+library(sf)
+
+variables <- load_variables(2017,'acs5')
+
+write.csv(variables,'C:/Users/richa/OneDrive/Documents/School/UPENN/Fall_2023/Public Policy Analytics/Week2/DC_Transport_Project/variables.csv')
+
+acs_variables <- c('B25026_001', #Total Population
+                   'B05003B_001', #African American Pop
+                   'B05003A_001', #White Population
+                   'B05003C_001', #Alaskan and Native American
+                   'B05003D_001', #Asian
+                   'B05003E_001', #Native Hawian or Pacfic Islander
+                   'B05003F_001', #Other
+                   'B05003G_001', #Two Races
+                   'B19013_001', #Median Income
+                   'B25058_001') #Rent
+
+census_api_key('4b917a3ebce557bc497aebb5aba1b04f0ff9c5ba',overwrite = FALSE)
+
+
+dc2017tracts <- get_acs('tract',
+                        variables = acs_variables,
+                        year = 2017,
+                        state = 'DC',
+                        geometry = TRUE,
+                        output = 'wide')%>%
+  
+  rename('Total_pop' = 'B25026_001E',
+         'African_pop' = 'B05003B_001E',
+         'White_pop' = 'B05003A_001E',
+         'Alaskan_native_pop' = 'B05003C_001E',
+         'Asian_pop'='B05003D_001E',
+         'Native_hawian_pop'='B05003E_001E',
+         'Other_pop'='B05003F_001E',
+         'Two_races_pop'='B05003G_001E',
+         'Median_income'='B19013_001E',
+         'Median_rent'='B25058_001E') %>%
+  st_transform('EPSG:2248') %>%
+  dplyr::select(-NAME, -starts_with("B")) %>%
+  mutate(totalpop2 = African_pop + White_pop + Alaskan_native_pop + Asian_pop + Native_hawian_pop + Other_pop + Two_races_pop)
+
+dc_boundary <- st_union(dc2017tracts)
+
+dcmetro <- st_read('https://maps2.dcgis.dc.gov/dcgis/rest/services/DCGIS_DATA/Transportation_Rail_Bus_WebMercator/MapServer/51/query?outFields=*&where=1%3D1&f=geojson')
+dcmetro <- st_transform('EPSG:2248') 
+dcmetro <- dcmetro[st_intersects(dcmetro, dc_boundary) %>% lengths > 0, ]
+
+ggplot()+
+  geom_sf(data = dc_boundary)+
+  geom_sf(data = dcmetro)
