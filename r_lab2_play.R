64a0d1c9344067b284e0c2ec1d356f134ef632c4
Richard Barad <richard.barad@gmail.com>
2023-09-13 12:55:07 -0400

    Update with new work with Alec and Nohman
    Work with Alec and Nohman on 9/13/2023

--- r_lab2_play.R
+++ r_lab2_play.R
@@ -9,60 +9,53 @@ census_api_key('4b917a3ebce557bc497aebb5aba1b04f0ff9c5ba',overwrite = FALSE)
 variables2017 <- load_variables(2017,'acs5')
 variables2009 <- load_variables(2009,'acs5')
 
-write.csv(variables,'variables.csv')
+acs_variables2017 <- c(
+                   'B19013_001', #Median Household Income
+                   'B25058_001', #Rent
+                   'B27011_003', #Total Working Population - not in 2009
+                   'B08126_014', #Number of People Working in Public Adminstration - not in 2009
+                   'B08301_010', #Number of People Commuting in any form of public transit
+                   'B25026_001') #Total Pop
 
-acs_variables <- c('B05003_001', #Total Population
-                   'B05003B_001', #African American Pop
-                   'B05003A_001', #White Population
-                   'B05003C_001', #Alaskan and Native American
-                   'B05003D_001', #Asian
-                   'B05003E_001', #Native Hawian or Pacfic Islander
-                   'B05003F_001', #Other
-                   'B05003G_001', #Two Races
-                   'B19013_001', #Median Income
-                   'B25058_001') #Rent 
+acs_variables2009 <- c(
+  'B19013_001', #Median Household Income
+  'B25058_001', #Rent
+  'B24080_001', #Total Working Population - not in 2009 #This data is at the block level in 2017, will tidycensus aggregate for us to tract?
+  'C24030_028', #Male Population in Public Administration
+  'B08301_010', #Number of People Commuting in any form of public transit
+  'B25026_001', #Total Pop
+  'C24030_055') #Female Population in Public Administration
 
 dc2017tracts <- get_acs('tract',
-                        variables = acs_variables,
+                        variables = acs_variables2017,
                         year = 2017,
                         state = 'DC',
                         geometry = TRUE,
                         output = 'wide') %>%
-  rename('Total_pop' = 'B05003_001E',
-         'Black_pop' = 'B05003B_001E',
-         'White_pop' = 'B05003A_001E',
-         'Alaskan_native_pop' = 'B05003C_001E',
-         'Asian_pop'='B05003D_001E',
-         'Native_hawian_pop'='B05003E_001E',
-         'Other_pop'='B05003F_001E',
-         'Two_races_pop'='B05003G_001E',
-         'Median_income'='B19013_001E',
-         'Median_rent'='B25058_001E') %>%
+  rename('Median_income'='B19013_001E',
+         'Median_rent'='B25058_001E',
+         'Working_Pop' = 'B27011_003E',
+         'public_admin_pop' = 'B08126_014E',
+         'commute_public_transit' = 'B08301_010E',
+         'total_pop' = 'B25026_001E',) %>%
   st_transform('EPSG:2248') %>%
-  dplyr::select(-NAME, -starts_with("B0"), -starts_with("B1")) %>%
-  mutate(pct_white = (White_pop / Total_pop) * 100,
-         pct_nonwhite = ((Black_pop + Alaskan_native_pop + Asian_pop + Native_hawian_pop + Other_pop + Two_races_pop) / Total_pop) * 100)
+  dplyr::select(-NAME, -starts_with("B0"), -starts_with("B1"), -starts_with("B2"),-starts_with("C2"))
 
 dc2009tracts <- get_acs('tract',
-                        variables = acs_variables,
+                        variables = acs_variables2009,
                         year = 2009,
                         state = 'DC',
                         geometry = TRUE,
                         output = 'wide') %>%
-  rename('Total_pop' = 'B05003_001E',
-         'Black_pop' = 'B05003B_001E',
-         'White_pop' = 'B05003A_001E',
-         'Alaskan_native_pop' = 'B05003C_001E',
-         'Asian_pop'='B05003D_001E',
-         'Native_hawian_pop'='B05003E_001E',
-         'Other_pop'='B05003F_001E',
-         'Two_races_pop'='B05003G_001E',
-         'Median_income'='B19013_001E',
-         'Median_rent'='B25058_001E') %>%
+  rename('Median_income'='B19013_001E',
+         'Median_rent'='B25058_001E',
+         'Working_Pop' = 'B24080_001E',
+         'male_public_admin_pop' = 'C24030_028E',
+         'female_public_admin_pop' = 'C24030_055E',
+         'commute_public_transit' = 'B08301_010E',
+         'total_pop' = 'B25026_001E') %>%
   st_transform('EPSG:2248') %>%
-  dplyr::select(-NAME, -starts_with("B0"), -starts_with("B1")) %>%
-  mutate(pct_white = (White_pop / Total_pop) * 100,
-         pct_nonwhite = ((Black_pop + Alaskan_native_pop + Asian_pop + Native_hawian_pop + Other_pop + Two_races_pop) / Total_pop) * 100)
+  dplyr::select(-NAME, -starts_with("B0"), -starts_with("B1"), -starts_with("B2"),-starts_with("C2"))
 
 dc_boundary <- st_union(dc2017tracts)
 
