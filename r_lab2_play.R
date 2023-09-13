2a67b8ea16fd666ee719eb671a9f193f2a150a8b
Richard Barad <richard.barad@gmail.com>
2023-09-12 23:44:26 -0400

    Update code
    Add codes to get data for two different acs years.

--- r_lab2_play.R
+++ r_lab2_play.R
@@ -1,13 +1,17 @@
+
 library(tidycensus)
 library(tidyverse)
 library(kableExtra)
 library(sf)
 
-variables <- load_variables(2017,'acs5')
+census_api_key('4b917a3ebce557bc497aebb5aba1b04f0ff9c5ba',overwrite = FALSE)
 
-write.csv(variables,'C:/Users/richa/OneDrive/Documents/School/UPENN/Fall_2023/Public Policy Analytics/Week2/DC_Transport_Project/variables.csv')
+variables2017 <- load_variables(2017,'acs5')
+variables2009 <- load_variables(2009,'acs5')
 
-acs_variables <- c('B25026_001', #Total Population
+write.csv(variables,'variables.csv')
+
+acs_variables <- c('B05003_001', #Total Population
                    'B05003B_001', #African American Pop
                    'B05003A_001', #White Population
                    'B05003C_001', #Alaskan and Native American
@@ -16,10 +20,7 @@ acs_variables <- c('B25026_001', #Total Population
                    'B05003F_001', #Other
                    'B05003G_001', #Two Races
                    'B19013_001', #Median Income
-                   'B25058_001') #Rent
-
-census_api_key('4b917a3ebce557bc497aebb5aba1b04f0ff9c5ba',overwrite = FALSE)
-
+                   'B25058_001') #Rent 
 
 dc2017tracts <- get_acs('tract',
                         variables = acs_variables,
@@ -27,8 +28,8 @@ dc2017tracts <- get_acs('tract',
                         state = 'DC',
                         geometry = TRUE,
                         output = 'wide') %>%
-  rename('Total_pop' = 'B25026_001E',
-         'African_pop' = 'B05003B_001E',
+  rename('Total_pop' = 'B05003_001E',
+         'Black_pop' = 'B05003B_001E',
          'White_pop' = 'B05003A_001E',
          'Alaskan_native_pop' = 'B05003C_001E',
          'Asian_pop'='B05003D_001E',
@@ -38,13 +39,35 @@ dc2017tracts <- get_acs('tract',
          'Median_income'='B19013_001E',
          'Median_rent'='B25058_001E') %>%
   st_transform('EPSG:2248') %>%
-  dplyr::select(-NAME, -starts_with("B")) %>%
-  mutate(totalpop2 = African_pop + White_pop + Alaskan_native_pop + Asian_pop + Native_hawian_pop + Other_pop + Two_races_pop)
+  dplyr::select(-NAME, -starts_with("B0"), -starts_with("B1")) %>%
+  mutate(pct_white = (White_pop / Total_pop) * 100,
+         pct_nonwhite = ((Black_pop + Alaskan_native_pop + Asian_pop + Native_hawian_pop + Other_pop + Two_races_pop) / Total_pop) * 100)
+
+dc2009tracts <- get_acs('tract',
+                        variables = acs_variables,
+                        year = 2009,
+                        state = 'DC',
+                        geometry = TRUE,
+                        output = 'wide') %>%
+  rename('Total_pop' = 'B05003_001E',
+         'Black_pop' = 'B05003B_001E',
+         'White_pop' = 'B05003A_001E',
+         'Alaskan_native_pop' = 'B05003C_001E',
+         'Asian_pop'='B05003D_001E',
+         'Native_hawian_pop'='B05003E_001E',
+         'Other_pop'='B05003F_001E',
+         'Two_races_pop'='B05003G_001E',
+         'Median_income'='B19013_001E',
+         'Median_rent'='B25058_001E') %>%
+  st_transform('EPSG:2248') %>%
+  dplyr::select(-NAME, -starts_with("B0"), -starts_with("B1")) %>%
+  mutate(pct_white = (White_pop / Total_pop) * 100,
+         pct_nonwhite = ((Black_pop + Alaskan_native_pop + Asian_pop + Native_hawian_pop + Other_pop + Two_races_pop) / Total_pop) * 100)
 
 dc_boundary <- st_union(dc2017tracts)
 
 dcmetro <- st_read('https://maps2.dcgis.dc.gov/dcgis/rest/services/DCGIS_DATA/Transportation_Rail_Bus_WebMercator/MapServer/51/query?outFields=*&where=1%3D1&f=geojson') %>%
-  st_transform('EPSG:2248') 
+  st_transform('EPSG:2248')
 dcmetro <- dcmetro[st_intersects(dcmetro, dc_boundary) %>% lengths > 0, ]
 
 ggplot()+
