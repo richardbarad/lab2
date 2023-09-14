f3dd38e1af358f49bab6b73f2ae2769b7e562dbd
Richard Barad <richard.barad@gmail.com>
2023-09-14 16:36:35 -0400

    Add code to join 2017 and 2009 variables
    Join 2017 and 2009 variables together to get a list of variables in both datasets.

--- r_lab2_play.R
+++ r_lab2_play.R
@@ -9,6 +9,9 @@ census_api_key('4b917a3ebce557bc497aebb5aba1b04f0ff9c5ba',overwrite = FALSE)
 variables2017 <- load_variables(2017,'acs5')
 variables2009 <- load_variables(2009,'acs5')
 
+variables_join <- inner_join(variables2017,variables2009, by='name','suffix'=c('_2017','_2009')) %>%
+  dplyr::filter(geography == 'tract')
+
 acs_variables2017 <- c(
                    'B19013_001', #Median Household Income
                    'B25058_001', #Rent
