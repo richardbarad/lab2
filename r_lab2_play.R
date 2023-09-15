2de144b5b270a4276fc16d27ae8041fa14d4d5f5
Richard Barad <richard.barad@gmail.com>
2023-09-14 22:47:57 -0400

    Update r_lab2_play.R

--- r_lab2_play.R
+++ r_lab2_play.R
@@ -12,53 +12,60 @@ variables2009 <- load_variables(2009,'acs5')
 variables_join <- inner_join(variables2017,variables2009, by='name','suffix'=c('_2017','_2009')) %>%
   dplyr::filter(geography == 'tract')
 
-acs_variables2017 <- c(
-                   'B19013_001', #Median Household Income
+acs_variables <- c(
+                   'B25119_001', #Median Household Income
                    'B25058_001', #Rent
-                   'B27011_003', #Total Working Population - not in 2009
-                   'B08126_014', #Number of People Working in Public Adminstration - not in 2009
-                   'B08301_010', #Number of People Commuting in any form of public transit
-                   'B25026_001') #Total Pop
+                   'B08130_001', #Total Working Population
+                   'B08130_016', #Total Population Commuting on Public Transit
+                   'B17009_002', #Total Population with Income Below Poverty Level
+                   'B17009_001') #Total Population
 
-acs_variables2009 <- c(
-  'B19013_001', #Median Household Income
-  'B25058_001', #Rent
-  'B24080_001', #Total Working Population - not in 2009 #This data is at the block level in 2017, will tidycensus aggregate for us to tract?
-  'C24030_028', #Male Population in Public Administration
-  'B08301_010', #Number of People Commuting in any form of public transit
-  'B25026_001', #Total Pop
-  'C24030_055') #Female Population in Public Administration
+
+options(scipen=999) #
+options(tigris_class = "sf")
+
+source("https://raw.githubusercontent.com/urbanSpatial/Public-Policy-Analytics-Landing/master/functions.r")
+
+palette5 <- c("#f0f9e8","#bae4bc","#7bccc4","#43a2ca","#0868ac")
+
 
 dc2017tracts <- get_acs('tract',
-                        variables = acs_variables2017,
+                        variables = acs_variables,
                         year = 2017,
                         state = 'DC',
                         geometry = TRUE,
                         output = 'wide') %>%
-  rename('Median_income'='B19013_001E',
-         'Median_rent'='B25058_001E',
-         'Working_Pop' = 'B27011_003E',
-         'public_admin_pop' = 'B08126_014E',
-         'commute_public_transit' = 'B08301_010E',
-         'total_pop' = 'B25026_001E',) %>%
+  rename('median_income'='B25119_001E',
+         'median_rent'='B25058_001E',
+         'working_pop' = 'B08130_001E',
+         'transit_pop' = 'B08130_016E',
+         'poverty_pop' = 'B17009_002E',
+         'total_pop' = 'B17009_001E',) %>%
   st_transform('EPSG:2248') %>%
-  dplyr::select(-NAME, -starts_with("B0"), -starts_with("B1"), -starts_with("B2"),-starts_with("C2"))
+  mutate(pct_transit = (transit_pop / working_pop) * 100,
+         pct_poverty = (poverty_pop / total_pop) * 100,
+         year = '2017') %>%
+  dplyr::select(-NAME, -starts_with("B0"), -starts_with("B1"), -starts_with("B2"))
 
 dc2009tracts <- get_acs('tract',
-                        variables = acs_variables2009,
+                        variables = acs_variables,
                         year = 2009,
                         state = 'DC',
                         geometry = TRUE,
                         output = 'wide') %>%
-  rename('Median_income'='B19013_001E',
-         'Median_rent'='B25058_001E',
-         'Working_Pop' = 'B24080_001E',
-         'male_public_admin_pop' = 'C24030_028E',
-         'female_public_admin_pop' = 'C24030_055E',
-         'commute_public_transit' = 'B08301_010E',
-         'total_pop' = 'B25026_001E') %>%
+  rename('median_income'='B25119_001E',
+         'median_rent'='B25058_001E',
+         'working_pop' = 'B08130_001E',
+         'transit_pop' = 'B08130_016E',
+         'poverty_pop' = 'B17009_002E',
+         'total_pop' = 'B17009_001E',) %>%
   st_transform('EPSG:2248') %>%
-  dplyr::select(-NAME, -starts_with("B0"), -starts_with("B1"), -starts_with("B2"),-starts_with("C2"))
+  mutate(pct_transit = (transit_pop / working_pop) * 100,
+         pct_poverty = (poverty_pop / total_pop) * 100,
+         year = '2009') %>%
+  dplyr::select(-NAME, -starts_with("B0"), -starts_with("B1"), -starts_with("B2"))
+
+census_bind = rbind(dc2009tracts, dc2017tracts)
 
 dc_boundary <- st_union(dc2017tracts)
 
@@ -67,5 +74,9 @@ dcmetro <- st_read('https://maps2.dcgis.dc.gov/dcgis/rest/services/DCGIS_DATA/Tr
 dcmetro <- dcmetro[st_intersects(dcmetro, dc_boundary) %>% lengths > 0, ]
 
 ggplot()+
-  geom_sf(data = dc_boundary)+
-  geom_sf(data = dcmetro)
+  geom_sf(data = census_bind, aes(fill = q5(median_income)))+
+  #geom_sf(data = dcmetro)+
+  facet_wrap(~year)+
+  scale_fill_manual(values = palette5,
+                    name = "Median Income\n(Quintile Breaks)")+
+  mapTheme()
